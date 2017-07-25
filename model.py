6a939bf6d84d16a2fc6d793513db585c3b8a33a5
wjh720 <wjh720.eric@gmail.com>
2017-07-25 16:06:16 +0800

    Add

--- model.py
+++ model.py
@@ -45,7 +45,8 @@ class Learner():
 
     def learn(self):
         tbCallBack = keras.callbacks.TensorBoard(log_dir='../Graph', histogram_freq=0, write_graph=True, write_images=True)
-        checkpointer = ModelCheckpoint(filepath='/data/tmpsrt1/log_new/wph', save_best_only=True, period = 10, verbose = 1,save_weights_only = True)
+        checkpointer = ModelCheckpoint(filepath='/data/tmpsrt1/log_new/wph', save_best_only=True, period = 10, \
+                        verbose = 1,save_weights_only = True)
         
         print(' Begin fitting ')
 
@@ -71,4 +72,7 @@ class Learner():
         create_model()
         learn()
 
-a = Learner.work()
+a = Learner()
+a.work()
+
+
