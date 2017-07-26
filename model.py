659a5ccc072da29b87306745bb85100c2a9fae10
wjh720 <wjh720.eric@gmail.com>
2017-07-26 13:35:22 +0800

    Staff

--- model.py
+++ model.py
@@ -54,7 +54,11 @@ class Learner():
             #print(asd.shape)
             norm_asd = asd - np.mean(asd, axis = 0)
             #print(np.mean(norm_asd, axis = 0))
-            #print(np.std(norm_asd, axis = 0))
+            print(np.std(norm_asd, axis = 0))
+            ww = np.std(norm_asd, axis = 0)
+            qe = ww[np.where(ww == 0)]
+            print(qe.shape)
+            time.sleep(3)
             #norm_asd = norm_asd / np.std(norm_asd, axis = 0)
             #print(np.std(norm_asd, axis = 0))
 
