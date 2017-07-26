08f98c39ab1dd74c7405b5f7443f78024e39b835
wjh720 <wjh720.eric@gmail.com>
2017-07-27 03:06:15 +0800

    Asdf

--- mkdata.py
+++ mkdata.py
@@ -96,34 +96,54 @@ def prepare_mfcc():
     print(num_label)
 
     if (overwrite):
-        data = []
+        data_1 = []
+        data_2 = []
+        data_3 = []
         num = 0
         for item in file_list:
             #print(item)
             y, sr=soundfile.read(item)
             y = np.mean(y.T, axis=0)
 
-            D = np.abs(librosa.stft(y)) ** 2
-            #print(D.shape)
-            S = librosa.feature.melspectrogram(S = D)
+            D_1 = np.abs(librosa.stft(y, n_fft = 1024)) ** 2
+            D_2 = np.abs(librosa.stft(y, n_fft = 2048)) ** 2
+            D_3 = np.abs(librosa.stft(y, n_fft = 4096)) ** 2
+            
+            S_1 = librosa.feature.melspectrogram(S = D_1)
+            S_2 = librosa.feature.melspectrogram(S = D_2)
+            S_3 = librosa.feature.melspectrogram(S = D_3)
             #print(S.shape)
 
-            S = librosa.feature.mfcc(S=librosa.power_to_db(S), n_mfcc = 64)
+            S_1 = librosa.feature.mfcc(S=librosa.power_to_db(S_1), n_mfcc = 64)
+            S_2 = librosa.feature.mfcc(S=librosa.power_to_db(S_2), n_mfcc = 64)
+            S_3 = librosa.feature.mfcc(S=librosa.power_to_db(S_3), n_mfcc = 64)
             #print(S.shape)
             #time.sleep(1000)
 
-            data.append(S.T)
+            data_1.append(S_1.T)
+            data_2.append(S_2.T)
+            data_3.append(S_3.T)
 
             if (num % 100 == 0):
                 print(num)
             num = num + 1
 
-        data = np.array(data)
-        print(data.shape)
+        data_1 = np.array(data_1)
+        data_2 = np.array(data_2)
+        data_3 = np.array(data_3)
+        print(data_1.shape)
+        print(data_2.shape)
+        print(data_3.shape)
 
-        f = file('data_mfcc.npy', 'w')
-        np.save(f, data)
-        f.close()
+        f_1 = file('data_mfcc_1024.npy', 'w')
+        f_2 = file('data_mfcc_2048.npy', 'w')
+        f_3 = file('data_mfcc_4096.npy', 'w')
+        np.save(f_1, data_1)
+        np.save(f_2, data_2)
+        np.save(f_3, data_3)
+        f_1.close()
+        f_2.close()
+        f_3.close()
         print(' Data End ')
 
 #prepare_data()
