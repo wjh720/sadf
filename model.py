b55c0fadc61f86e3fe4baff04f9bd58c0067694d
wjh720 <wjh720.eric@gmail.com>
2017-07-27 00:16:52 +0800

    Staff

--- model.py
+++ model.py
@@ -159,11 +159,11 @@ class Learner():
 
         mfcc_reshape = Reshape((86, 128, 1))(mfcc)
         Conv_1 = Conv2D(64, (3, 3), padding='same', activation='relu')
-        Conv_2 = Conv2D(64, (3, 3), padding='same', activation='relu')
+        #Conv_2 = Conv2D(64, (3, 3), padding='same', activation='relu')
 
         conv_1 = Conv_1(mfcc_reshape)
-        conv_1_bh = BatchNormalization()(conv_1)
-        conv_2 = Conv_2(conv_1_bh)
+        #conv_1_bh = BatchNormalization()(conv_1)
+        #conv_2 = Conv_2(conv_1_bh)
         maxpool_1 = MaxPooling2D(pool_size = (3, 3))(conv_1)
         drop_1 = Dropout(0.1)(maxpool_1)
 
@@ -174,11 +174,11 @@ class Learner():
         conv_3 = Conv_3(drop_1)
         conv_3_bh = BatchNormalization()(conv_3)
 
-        concat_1 = conv_3_bh
+        concat_1 = Concatenate(axis = 3)([drop_1, conv_3_bh])
         conv_4 = Conv_4(concat_1)
         conv_4_bh = BatchNormalization()(conv_4)
 
-        concat_2 = conv_4_bh
+        concat_2 = Concatenate(axis = 3)([drop_1, conv_3_bh, conv_4_bh])
         conv_01 = Conv_01(concat_2)
 
         maxpool_2 = MaxPooling2D(pool_size = (3, 3))(conv_01)
@@ -191,11 +191,11 @@ class Learner():
         conv_5 = Conv_5(drop_2)
         conv_5_bh = BatchNormalization()(conv_5)
 
-        concat_3 = conv_5_bh
+        concat_3 = Concatenate(axis = 3)([drop_2, conv_5_bh])
         conv_6 = Conv_6(concat_3)
         conv_6_bh = BatchNormalization()(conv_6)
 
-        concat_4 = conv_6_bh
+        concat_4 = Concatenate(axis = 3)([drop_2, conv_5_bh, conv_6_bh])
         conv_02 = Conv_02(concat_4)
 
         lam_1 = Lambda(lam, output_shape=(14, 64))(conv_02)
