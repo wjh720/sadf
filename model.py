54d065b5ebc1403d4d5e0f00d62fcc57258f53aa
wjh720 <wjh720.eric@gmail.com>
2017-07-26 15:54:09 +0800

    qer

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ import tensorflow as tf
 from keras.callbacks import ModelCheckpoint
 import keras
 
-num_repeat = 10
+num_repeat = 19
 num_asd = 25
 num_classes = 15
 
@@ -157,13 +157,11 @@ class Learner():
         pdata = []
         for i in range(n):
             asd = self.data[i]
-            norm_asd = asd - np.mean(asd, axis = 0)
-            norm_asd = norm_asd / np.std(norm_asd, axis = 0)
 
             for j in range(num_repeat):
-                Start = j * 86
-                End = (j + 1) * 86
-                aa = norm_asd[Start : End]
+                Start = j * 43
+                End = (j + 2) * 86
+                aa = asd[Start : End]
                 #print(aa.shape)
                 pdata.append(aa)
 
