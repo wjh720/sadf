ccaeedde6d4bef7b2a8fdcf67241943b3a085043
wjh720 <wjh720.eric@gmail.com>
2017-08-02 17:49:43 +0800

    Ask

--- model_77_78_2.py
+++ model_77_78_2.py
@@ -318,11 +318,11 @@ class Learner():
         Conv_3_7 = Conv2D(64, (K_n, K_n), padding='same', activation='relu')
         Conv_3_8 = Conv2D(64, (K_n, K_n), padding='same', activation='relu')
 
-        conv_1_7 = Conv_1_7(conv_1_in_1)
+        conv_1_7 = Conv_1_7(conv_1_in_3)
         conv_1_8 = Conv_1_8(conv_1_7)
-        conv_2_7 = Conv_2_7(conv_2_in_1)
+        conv_2_7 = Conv_2_7(conv_2_in_3)
         conv_2_8 = Conv_2_8(conv_2_7)
-        conv_3_7 = Conv_3_7(conv_3_in_1)
+        conv_3_7 = Conv_3_7(conv_3_in_3)
         conv_3_8 = Conv_3_8(conv_3_7)
 
         lam_1 = Lambda(lam, output_shape=(32, 64))(conv_1_8)
@@ -459,8 +459,8 @@ class Learner():
     def work(self):
         self.prepare()
         self.create_mfcc()
-        #self.learn()
-        self.predict()
+        self.learn()
+        #self.predict()
 
 a = Learner()
 a.work()
